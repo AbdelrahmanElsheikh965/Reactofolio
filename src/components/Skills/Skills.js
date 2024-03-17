97340bfabfed9f27c8b67dbde84beefe56d29998
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-03-17 14:54:19 +0200

    Added rendering lists

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,6 +1,21 @@
 import React from "react";
 
 function Skills() {
+
+    const skillsArray_1 = [
+        {title: 'C/C++', ratio: '60',  style: {width: '60%', height: '8px'}},
+        {title: 'PHP', ratio: '80'   , style: {width: '80%', height: '8px'}},
+        {title: 'Java', ratio: '100' , style: {width: '100%', height: '8px'}},
+        {title: 'Js', ratio: '70'    , style: {width: '70%', height: '8px'}},
+    ];
+
+    const skillsArray_2 = [
+        {title: 'Nodejs', ratio: '65',  style: {width: '65%', height: '8px'}},
+        {title: 'Laravel', ratio: '85',  style: {width: '85%', height: '8px'}},
+        {title: 'Nginx', ratio: '60',  style: {width: '60%', height: '8px'}},
+        {title: 'Docker', ratio: '70',  style: {width: '70%', height: '8px'}},
+    ];
+
   return <section id="skills" class="skills">
   <div class="skill-content">
       <div class="section-heading text-center">
@@ -10,98 +25,42 @@ function Skills() {
           <div class="row">
               <div class="col-md-6">
                   <div class="single-skill-content">
-                      <div class="barWrapper">
-                          <span class="progressText">adobe photoshop</span>
-                          <div class="single-progress-txt">
-                              <div class="progress ">
-                                  <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="10" aria-valuemax="100"  >
-                                        
-                                  </div>
-                              </div>
-                              <h3>90%</h3>	
-                          </div>
-                      </div>  
-                      <div class="barWrapper">
-                          <span class="progressText">adobe illustrator</span>
-                          <div class="single-progress-txt">
-                              <div class="progress ">
-                                 <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="10" aria-valuemax="100"  >
-                                      
+                    
+                    {skillsArray_1.map((skill) => (
+                         <div class="barWrapper">
+                         <span class="progressText">{skill.title}</span>
+                         <div class="single-progress-txt">
+                             <div class="progress ">
+                                 <div class="progress-bar" role="progressbar" aria-valuenow="90" 
+                                       aria-valuemin="10" aria-valuemax="100" style={skill.style} >
                                  </div>
-                              </div>
-                              <h3>85%</h3>	
-                          </div>
-                      </div>  
-                      <div class="barWrapper">
-                          <span class="progressText">adobe after effects</span>
-                          <div class="single-progress-txt">
-                              <div class="progress ">
-                                 <div class="progress-bar" role="progressbar" aria-valuenow="97" aria-valuemin="10" aria-valuemax="100"  >
-                                     
-                                 </div>
-                              </div>
-                              <h3>97%</h3>	
-                          </div>
-                      </div>  
-                      <div class="barWrapper">
-                          <span class="progressText">sketch</span>
-                          <div class="single-progress-txt">
-                              <div class="progress ">
-                                 <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="10" aria-valuemax="100"  >
-                                      
-                                 </div>
-                              </div>
-                              <h3>90%</h3>	
-                          </div>
-                      </div>  
+                             </div>
+                             <h3>{skill.ratio}%</h3>	
+                         </div>
+                     </div> 
+                    ))}
+
+                     
+
                   </div>
               </div>
               <div class="col-md-6">
                   <div class="single-skill-content">
-                      <div class="barWrapper">
-                          <span class="progressText">html 5</span>
-                          <div class="single-progress-txt">
-                              <div class="progress ">
-                                  <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="10" aria-valuemax="100"  >
-                                      
-                                  </div>
-                              </div>
-                              <h3>90%</h3>	
-                          </div>
-                      </div>  
-                      <div class="barWrapper">
-                          <span class="progressText">css 3 animation</span>
-                          <div class="single-progress-txt">
-                              <div class="progress ">
-                                 <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="10" aria-valuemax="100"  >
-                                      
+
+                  {skillsArray_2.map((skill) => (
+                         <div class="barWrapper">
+                         <span class="progressText">{skill.title}</span>
+                         <div class="single-progress-txt">
+                             <div class="progress ">
+                                 <div class="progress-bar" role="progressbar" aria-valuenow="90" 
+                                       aria-valuemin="10" aria-valuemax="100" style={skill.style} >
                                  </div>
-                              </div>
-                              <h3>85%</h3>	
-                          </div>
-                      </div>  
-                      <div class="barWrapper">
-                          <span class="progressText">communication</span>
-                          <div class="single-progress-txt">
-                              <div class="progress ">
-                                 <div class="progress-bar" role="progressbar" aria-valuenow="97" aria-valuemin="10" aria-valuemax="100"  >
-                                     
-                                 </div>
-                              </div>
-                              <h3>97%</h3>	
-                          </div>
-                      </div>  
-                      <div class="barWrapper">
-                          <span class="progressText"> creativity</span>
-                          <div class="single-progress-txt">
-                              <div class="progress ">
-                                 <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="10" aria-valuemax="100"  >
-                                      
-                                 </div>
-                              </div>
-                              <h3>90%</h3>	
-                          </div>
-                      </div>  
+                             </div>
+                             <h3>{skill.ratio}%</h3>	
+                         </div>
+                     </div> 
+                    ))}
+                      
                   </div>
               </div>
           </div>
